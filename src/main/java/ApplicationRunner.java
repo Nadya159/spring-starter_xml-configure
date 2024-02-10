81ec262c76a7189d49a77fe676c81ff3893e2f79
Nadya159 <nadezdovna59@gmail.com>
2024-02-10 22:38:44 +0500

    add beans

---
+++ src/main/java/ApplicationRunner.java
@@ -0,0 +1,9 @@
+import database.pool.ConnectionPool;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        System.out.println(context.getBean(ConnectionPool.class));
+    }
+}
